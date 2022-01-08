c861edd3ddd3e91781dd44f1514f1b829dbdaa43
ptatap29 <86825374+ptatap29@users.noreply.github.com>
2022-01-08 15:34:44 +0530

    Update order.js

--- modules/order.js
+++ modules/order.js
@@ -1,1 +1,37 @@
+//Require Modules
+var mongoose = require("mongoose");
+const autoIncrement = require("mongoose-auto-increment");
 
+//Making Schema
+const retailerschema = new mongoose.Schema({
+    name: {
+        type : String
+    },
+    phone_number:{
+        type : Number
+         
+        
+    },
+    wholesalerId:{
+        type : Array
+    }
+
+},{timestamps:true})
+
+
+
+
+
+
+//Making Auto Increment
+autoIncrement.initialize(mongoose.connection);
+retailerschema.plugin(autoIncrement.plugin ,{
+    model:"retailerschema",
+    startedAt:1,
+    increasedBy:1
+})
+
+//Exporting Schema
+var retailer = mongoose.model('retailer' , retailerschema)
+
+module.exports = {retailer}
