74bd231492ae7fa36b05206ce8169c413c5bd70e
ptatap29 <86825374+ptatap29@users.noreply.github.com>
2022-01-08 15:35:34 +0530

    Update order.js

--- routers/order.js
+++ routers/order.js
@@ -1,1 +1,22 @@
+//requiring basic module
+const express = require("express")
+const router =  express.Router();
+
+const {newOrder , turnOver , getMaximumTurnOver} = require("../../assingment/controlers/order")
+
+
+//router to make order 
+router.post("/neworder/:wholesalerid/:retailerid" , newOrder);
+
+
+//router to get total monthly turnover of each wholesaler for a complete year
+router.get("/turnover/:wholesalerid" ,  turnOver )
+
+
+//for getting maximum turnover from a single retailer
+router.get("/getmaximumturnover" , getMaximumTurnOver)
+
+
+module.exports = router
+
 
